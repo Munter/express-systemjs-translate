b4857eff726e5ff99125280d1a66701074156beb
Gustav Nikolaj Olsen <gno@one.com>
2016-10-24 09:53:43 +0200

    use the customAssertion

--- test/maybeRealPath-test.js
+++ test/maybeRealPath-test.js
@@ -1,25 +1,28 @@
 var path = require('path');
 var proxyquire = require('proxyquire');
-var expect = require('unexpected')
-  .clone()
-  .addAssertion('<string> to resolve to <string>', function (expect, subject, value) {
-    return expect(subject, 'to equal', value);
-  });
-var maybeRealPath = require('../lib/maybeRealPath');
 
 function fixturesPath(name) {
   return path.resolve(__dirname, '../fixtures/maybeRealPath', name);
 }
 
+
+var expect = require('unexpected')
+  .clone()
+  .addAssertion('<string> to resolve to <string>', function (expect, subject, value) {
+    return expect(maybeRealPath(fixturesPath(subject)), 'to equal', fixturesPath(value));
+  });
+var maybeRealPath = require('../lib/maybeRealPath');
+
+
 describe('maybeRealPath', function () {
   it('should return the original path to an existing file', function () {
-    return expect(maybeRealPath(fixturesPath('file.txt')), 'to resolve to', fixturesPath('file.txt'));
+    return expect('file.txt', 'to resolve to', 'file.txt');
   });
   it('should resolve a symlink to the original path', function () {
-    return expect(maybeRealPath(fixturesPath('symlink.txt')), 'to resolve to', fixturesPath('file.txt'));
+    return expect('symlink.txt', 'to resolve to', 'file.txt');
   });
   it('should return the path to the symlink when resolving a broken symlink', function () {
-    return expect(maybeRealPath(fixturesPath('brokenSymlink.txt')), 'to resolve to', fixturesPath('brokenSymlink.txt'));
+    return expect('brokenSymlink.txt', 'to resolve to', 'brokenSymlink.txt');
   });
   it('should swallow ENOENT exceptions and return the input instead', function () {
     var maybeRealPath = proxyquire('../lib/maybeRealPath', {
